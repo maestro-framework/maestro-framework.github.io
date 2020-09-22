fef235c8533eeb83dd1258b31ea5031eb6a2e611
John Isom <John.Isom2001@gmail.com>
2020-09-21 18:51:38 -0600

    Complete slideshow JavaScript

--- assets/scripts/slideshow.js
+++ assets/scripts/slideshow.js
@@ -1,17 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
   const slideshow = document.getElementById("console-slideshow");
-  const prevButton = slideshow.getElementsByClassName("prev-button")[0];
-  const nextButton = slideshow.getElementsByClassName("next-button")[0];
   const slides = slideshow.getElementsByClassName("slideshow-items")[0].children;
-  
+  let visibleIdx = 0;
+
+  const prevButton = document.createElement("div");
+  prevButton.classList.add("prev-button");
+  slideshow.appendChild(prevButton);
+
+  const nextButton = document.createElement("div");
+  nextButton.classList.add("next-button");
+  slideshow.appendChild(nextButton);
+
+  const makeInvisible = (elem) => {
+    elem.style.display = "none";
+  };
+
+  const makeVisible = (elem) => {
+    elem.style.display = "";
+  };
+
+  hideAllSlidesButFirst = () => {
+    for (let i = 1; i < slides.length; i += 1) {
+      makeInvisible(slides[i]);
+    }
+  };
+
   const handlePreviousClick = (e) => {
-    console.log("event for previous click:", e);
+
+    makeInvisible(slides[visibleIdx]);
+
+    if (visibleIdx === 0) {
+      visibleIdx = slides.length - 1;
+    } else{
+      visibleIdx -= 1;
+    }
+
+    makeVisible(slides[visibleIdx]);
   };
 
   const handleNextClick = (e) => {
-    console.log("event for next click:", e);
+
+    makeInvisible(slides[visibleIdx]);
+
+    if (visibleIdx === slides.length - 1) {
+      visibleIdx = 0;
+    } else{
+      visibleIdx += 1;
+    }
+
+    makeVisible(slides[visibleIdx]);
   };
 
   prevButton.addEventListener("click", handlePreviousClick);
   nextButton.addEventListener("click", handleNextClick);
+
+  hideAllSlidesButFirst();
 });
